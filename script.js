94a9b130b9d4609a6ca39139b547c1d1292bbb61
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-05-02 10:52:36 +0200

    add: bonus snack 1

--- script.js
+++ script.js
@@ -16,3 +16,36 @@ const getPostTitle = (id) => {
 getPostTitle(19)
   .then((title) => console.log('post title: ', title))
   .catch((err) => console.log(err));
+
+// bonus
+
+const getPost = (id) => {
+  const fetchPost = new Promise((resolve, reject) => {
+    fetch(`https://dummyjson.com/posts/${id}`)
+      .then((response) => response.json())
+      .then((data) => resolve(data))
+      .catch((error) => reject(error));
+  });
+
+  return fetchPost;
+};
+
+const getAuthor = (userId) => {
+  const fetchAuthor = new Promise((resolve, reject) => {
+    fetch(`https://dummyjson.com/users/${userId}`)
+      .then((response) => response.json())
+      .then((data) => resolve(data))
+      .catch((error) => reject(error));
+  });
+
+  return fetchAuthor;
+};
+
+getPost(19)
+  .then((post) => {
+    console.table(post);
+    getAuthor(post.userId)
+      .then((author) => console.log('autore del post: ', author))
+      .catch((err) => console.log(err));
+  })
+  .catch((err) => console.log(err));
