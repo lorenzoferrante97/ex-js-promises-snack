3589bfd14ca73c36de470508da36d080b1f12f9c
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-05-02 12:03:33 +0200

    add: snack 2

--- script.js
+++ script.js
@@ -2,50 +2,81 @@
 
 // SNACK 1 --------------------------------------
 
-const getPostTitle = (id) => {
-  const fetchTitle = new Promise((resolve, reject) => {
-    fetch(`https://dummyjson.com/posts/${id}`)
-      .then((response) => response.json())
-      .then((data) => resolve(data.title))
-      .catch((error) => reject(error));
+// const getPostTitle = (id) => {
+//   const fetchTitle = new Promise((resolve, reject) => {
+//     fetch(`https://dummyjson.com/posts/${id}`)
+//       .then((response) => response.json())
+//       .then((data) => resolve(data.title))
+//       .catch((error) => reject(error));
+//   });
+
+//   return fetchTitle;
+// };
+
+// getPostTitle(19)
+//   .then((title) => console.log('post title: ', title))
+//   .catch((err) => console.log(err));
+
+// // bonus
+
+// const getPost = (id) => {
+//   const fetchPost = new Promise((resolve, reject) => {
+//     fetch(`https://dummyjson.com/posts/${id}`)
+//       .then((response) => response.json())
+//       .then((data) => resolve(data))
+//       .catch((error) => reject(error));
+//   });
+
+//   return fetchPost;
+// };
+
+// const getAuthor = (userId) => {
+//   const fetchAuthor = new Promise((resolve, reject) => {
+//     fetch(`https://dummyjson.com/users/${userId}`)
+//       .then((response) => response.json())
+//       .then((data) => resolve(data))
+//       .catch((error) => reject(error));
+//   });
+
+//   return fetchAuthor;
+// };
+
+// getPost(19)
+//   .then((post) => {
+//     console.table(post);
+//     getAuthor(post.userId)
+//       .then((author) => console.log('autore del post: ', author))
+//       .catch((err) => console.log(err));
+//   })
+//   .catch((err) => console.log(err));
+
+// SNACK 2 --------------------------------------
+
+let counterLanci = 0;
+
+const lanciaDado = (counterLanci) => {
+  const generateNum = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const risultato = Math.floor(Math.random() * 6) + 1;
+      if (counterLanci % 5 == 0) {
+        reject('ops! il dado è caduto dal tavolo, rilancio');
+      } else {
+        resolve(risultato);
+      }
+    }, 5000);
   });
 
-  return fetchTitle;
+  return generateNum;
 };
 
-getPostTitle(19)
-  .then((title) => console.log('post title: ', title))
-  .catch((err) => console.log(err));
-
-// bonus
-
-const getPost = (id) => {
-  const fetchPost = new Promise((resolve, reject) => {
-    fetch(`https://dummyjson.com/posts/${id}`)
-      .then((response) => response.json())
-      .then((data) => resolve(data))
-      .catch((error) => reject(error));
-  });
-
-  return fetchPost;
-};
-
-const getAuthor = (userId) => {
-  const fetchAuthor = new Promise((resolve, reject) => {
-    fetch(`https://dummyjson.com/users/${userId}`)
-      .then((response) => response.json())
-      .then((data) => resolve(data))
-      .catch((error) => reject(error));
-  });
-
-  return fetchAuthor;
-};
-
-getPost(19)
-  .then((post) => {
-    console.table(post);
-    getAuthor(post.userId)
-      .then((author) => console.log('autore del post: ', author))
-      .catch((err) => console.log(err));
-  })
-  .catch((err) => console.log(err));
+const giocoDadi = setInterval(() => {
+  console.log('lancio del dado...');
+  counterLanci++;
+  const lancio = lanciaDado(counterLanci);
+  lancio
+    .then((risDado) => {
+      console.log("E' uscito ", risDado);
+      counterLanci == 10 && clearInterval(giocoDadi);
+    })
+    .catch((err) => console.error(err));
+}, 3000);
