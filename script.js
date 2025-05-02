32a1665876a112a1024565ae33e738fe18b3ac0c
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-05-02 10:30:53 +0200

    add: snack 1

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // start code
+
+// SNACK 1 --------------------------------------
+
+const getPostTitle = (id) => {
+  const fetchTitle = new Promise((resolve, reject) => {
+    fetch(`https://dummyjson.com/posts/${id}`)
+      .then((response) => response.json())
+      .then((data) => resolve(data.title))
+      .catch((error) => reject(error));
+  });
+
+  return fetchTitle;
+};
+
+getPostTitle(19)
+  .then((title) => console.log('post title: ', title))
+  .catch((err) => console.log(err));
